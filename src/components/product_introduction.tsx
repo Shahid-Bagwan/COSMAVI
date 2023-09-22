f7d50fd3fc50d880546b988831fc1b435f077c54
Shahid Bagwan <112220551+Shahid-Bagwan@users.noreply.github.com>
2023-09-22 21:23:44 +0530

    Update product_introduction.tsx

--- src/components/product_introduction.tsx
+++ src/components/product_introduction.tsx
@@ -1,8 +1,9 @@
 import nanoplasty from "../assets/nanoplasty.png";
 const product_introduction = () => {
-  
+
   return (
     <>
+      {/* first section */}
       <div>
         <div className="w-full text-center">
           <h1 className="h1 text-3xl">Introducing NanoPlasty</h1>
@@ -16,23 +17,58 @@ const product_introduction = () => {
               className="h-96 w-28  "
             />
           </div>
-          <div className="text-left ml-12">
-            <div className="pt-7 pb-3 ml-3">
-              <h1 className="h1 text-xl">Introducing NanoPlasty</h1>
-              <p>World's best Product</p>
+          <div className="text-left ml-12 sm:flex sm:flex-col">
+            <div className="sm:flex sm:mb-16">
+              <div className="pt-7 pb-3 ml-3 sm:mr-8 border-b-2 border-gray-200 ">
+                <h1 className="h1 text-xl">Introducing NanoPlasty</h1>
+                <p>World's best Product</p>
+              </div>
+              <div className="sm:pt-7 pt-3 pb-3 ml-3 border-b-2 border-gray-200">
+                <h1 className="h1 text-xl">Introducing NanoPlasty</h1>
+                <p>World's best Product</p>
+              </div>
             </div>
-            <div className="sm:pt-7 pt-3 pb-3 ml-3">
-              <h1 className="h1 text-xl">Introducing NanoPlasty</h1>
-              <p>World's best Product</p>
+
+            <div className="sm:flex">
+              <div className="sm:pt-7 pt-3 pb-3 ml-3 sm:mr-8 border-b-2 border-gray-200">
+                <h1 className="h1 text-xl">Introducing NanoPlasty</h1>
+                <p>World's best Product</p>
+              </div>
+              <div className="sm:pt-7 pt-3 pb-3 ml-3 border-b-2 border-gray-200">
+                <h1 className="h1 text-xl">Introducing NanoPlasty</h1>
+                <p>World's best Product</p>
+              </div>
             </div>
-            <div className="sm:pt-7 pt-3 pb-3 ml-3">
-              <h1 className="h1 text-xl">Introducing NanoPlasty</h1>
-              <p>World's best Product</p>
+
+
+          </div>
+        </div>
+      </div>
+
+      {/* second section */}
+      <div className="flex flex-col  w-full">
+        <div className="">
+          <h1>
+            for professional saloons <br />
+            and women
+          </h1>
+        </div>
+        <div className="flex justify-center  flex-col sm:flex-row ">
+          <div className="sm:w-2/4 w-full text-left">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia quaerat dolore corporis adipisci, nobis soluta natus? Aspernatur vitae et ab, laborum, facere velit minus, accusamus ipsa exercitationem a ipsam magnam.</p>
+            <div className="flex flex-col ">
+              <div className="sm:pt-7 w-4/6 pt-3 pb-3   border-b-2 border-gray-200">
+                <h1 className="h1 text-xl">Introducing NanoPlasty</h1>
+                <p>World's best Product</p>
+              </div>
+              <div className="sm:pt-7 w-4/6 pt-3 pb-3  border-b-2 border-gray-200">
+                <h1 className="h1 text-xl">Introducing NanoPlasty</h1>
+                <p>World's best Product</p>
+              </div>
             </div>
-            <div className="sm:pt-7 pt-3 pb-3 ml-3">
-              <h1 className="h1 text-xl">Introducing NanoPlasty</h1>
-              <p>World's best Product</p>
-            </div>
+          </div>
+          <div>
+            <img src="https://w0.peakpx.com/wallpaper/518/289/HD-wallpaper-shahrukh-khan-srk-smile-srk-smile-thumbnail.jpg" alt="" className="sm:ml-8 h-96 w-full" />
           </div>
         </div>
       </div>
