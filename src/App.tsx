9e98b00a7d699d7cb7b9476fdc68c32b4df3e7bb
Shahid Bagwan <112220551+Shahid-Bagwan@users.noreply.github.com>
2023-09-25 13:48:49 +0530

    formated the code.

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,12 @@ function App() {
 
   return (
     <>
-    
-     <section className="container p-0 ">
       <Hero/>
-      </section>
-      <section className="container px-0 ">
       <Product/>
       <Steps_section/>
-      </section>
-      <section className="container px-0 ">
       <About/>
-      </section>
-      <section className="container px-0 ">
       <TeamComponent/>
-      </section>
-      <section className="container px-0 ">
       <Footer/>
-      </section>
-      
     </>
   )
 }
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -13,7 +13,8 @@ const Hero = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-      <div id='home' className="hero text-white bg-gradient-to-t from-white to-purple-500">
+      <section>
+        <div id='home' className="hero text-white  m-0 ">
         <header className="absolute inset-x-0 top-0 z-50">
           <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div className="flex lg:flex-1">
@@ -37,7 +38,7 @@ const Hero = () => {
             </div>
             <div className="hidden lg:flex lg:gap-x-12 pr-9">
               {navigation.map((item) => (
-                <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-700 hover:text-gray-800">
                   {item.name}
                 </a>
               ))}
@@ -46,10 +47,10 @@ const Hero = () => {
           </nav>
           <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
             <div className="fixed inset-0 z-50" />
-            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto  px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 bg-black">
               <div className="flex items-center justify-between">
                 <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only">Your Company</span>
+                  <span className="sr-only">Cosmavi</span>
                   <img
                     className="h-8 w-auto"
                     src=""
@@ -72,7 +73,7 @@ const Hero = () => {
                       <a
                         key={item.name}
                         href={item.href}
-                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-zinc-600"
                       >
                         {item.name}
                       </a>
@@ -98,7 +99,7 @@ const Hero = () => {
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
                   href="#"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="rounded-md bg-gradient-to-t from-green-500 to-green-200 hover:bg-gradient-to-b px-3.5 py-2.5 text-sm font-semibold text-gray-700 shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Our Products
                 </a>
@@ -111,6 +112,8 @@ const Hero = () => {
 
         </div>
       </div>
+      </section>
+      
     )
 
 }
